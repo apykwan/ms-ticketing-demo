5832b0c34a7af247683d60c37879ef09fc450de7
apykwan <apykwan@hotmail.com>
2025-09-26 05:26:16 -0700

    compelte current-user middleware to verify the jwt token and assign the payload to req.currentUser

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('John Doe');
+  req.session = null;
+
+  res.send({});
 });
 
 
