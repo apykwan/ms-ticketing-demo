206867c455c7db3cebca58322389fc6b623d5d88
apykwan <apykwan@hotmail.com>
2025-09-28 05:59:12 -0700

    add unit test for cookie after signing up an user

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,26 +11,35 @@ it('returns a 201 on successful signup', async() => {
     });
 
   expect(res.statusCode).toBe(201);
+  expect(res.body.email).toBe('test@test.com');
 });
 
 it('returns a 400 with an invalid email', async () => {
-  return request(app)
+  const res = await request(app)
     .post('/api/users/signup')
     .send({
       email: 'testtest.com',
       password: 'password'
     })
-    .expect(400);
+    
+  expect(res.statusCode).toBe(400);
+  expect(res.body.errors).toBeDefined();
+  expect(res.body.errors[0].message).toBe('Email must be provided');
+  expect(res.body.errors[0].field).toBe('email');
 });
 
 it('returns a 400 with an invalid password', async () => {
-  return request(app)
+  const res = await request(app)
     .post('/api/users/signup')
     .send({
       email: 'test@test.com',
       password: ''
-    })
-    .expect(400);
+    });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.errors).toBeDefined();
+    expect(res.body.errors[0].message).toBe('Password must be between 4 and 20 characters');
+    expect(res.body.errors[0].field).toBe('password');
 });
 
 it('returns a 400 with an invalid email and password', async () => {
@@ -52,11 +61,26 @@ it('returns a 400 with an invalid email and password', async () => {
 });
 
 it('disallows duplicate emails', async () => {
-  await request(app)
+  const res = await request(app)
     .post('/api/users/signup')
     .send({
       email: 'test@test.com',
       password: 'password'
-    })
-    .expect(400);
+    });
+    
+  expect(res.statusCode).toBe(400);
+  expect(res.body.errors).toBeDefined();
+  expect(res.body.errors[0].message).toBe('Email in use');
+});
+
+it('sets acookie after successful signup', async() => {
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test2@test.com',
+      password: 'password'
+    });
+
+  expect(res.statusCode).toBe(201);
+  expect(res.get('Set-Cookie')).toBeDefined();
 });
