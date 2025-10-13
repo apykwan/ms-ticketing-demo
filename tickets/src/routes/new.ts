1799f360e2ade44e2c48935336eaded092fe240c
apykwan <apykwan@hotmail.com>
2025-10-13 05:56:11 -0700

    add unit test for purchasing ticket

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,30 @@
 import express, { type Request, type Response } from 'express';
-import { requireAuth } from '@apkmstickets/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@apkmstickets/common';
+
+import { Ticket } from '@/models/ticket';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, async (req: Request, res: Response) => {
-  res.sendStatus(200);
+router.post(
+  '/api/tickets', 
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price be greater than zero')
+  ],
+  validateRequest, 
+  async (req: Request, res: Response) => {
+  const { title, price } = req.body;
+
+  const ticket = Ticket.build({
+    title, 
+    price,
+    userId: req.currentUser!.id
+  });
+
+  await ticket.save();
+  res.sendStatus(201);
 });
 
 export { router as createTicketRouter };
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,11 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 
 import { app } from '../app';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: MongoMemoryServer;
@@ -23,20 +24,25 @@ afterAll(async () => {
   await mongo.stop();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: '123456789',
+    email: 'test@test.com'
+  };
 
-  const res = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email, password
-    })
-    .expect(201)
-  
-  const cookie = res.get('Set-Cookie');
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  if (!cookie) throw new Error('Cookie is missing');
+  // Build session Object { jwt: MY_JWT }
+  const session = { jwt: token };
 
-  return cookie;
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and ecode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string that the cookie with the encoded data
+  return [`session=${base64}`];
 }
