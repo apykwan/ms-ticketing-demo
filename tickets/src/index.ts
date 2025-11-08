933df7be88d815e3e97984263fad26dd75083825
關沛宇 <guanpeiyu@MacBookAir.lan>
2025-11-08 04:26:13 -0800

    add nats-client-id, nats-url, nats-cluster-id to tickets-depl.yaml as env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,8 +8,15 @@ async function start () {
   try {
     if (!process.env.JWT_KEY) throw new Error('Invalid JWT');
     if (!process.env.MONGO_URI) throw new Error('MONGO_URI must be defined');
-    
-    await natsWrapper.connect('ticketing', 'adsf2343', 'http://nats-srv:4222');
+    if (!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID must be defined');
+    if (!process.env.NATS_URL) throw new Error('NATS_URL must be defined');
+    if (!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID must be defined');
+
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on('close', () => {
         console.log('NATS connectino closed');
         process.exit();
