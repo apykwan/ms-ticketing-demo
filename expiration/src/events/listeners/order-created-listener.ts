9119bc62123a95b6a7ceca3de33d7d99d68e853d
關沛宇 <guanpeiyu@MacBookAir.lan>
2025-12-18 05:50:18 -0800

    add expiration server and yaml file

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,13 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@apkmstickets/common';
+
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupName = queueGroupName;
+
+    onMessage (data: OrderCreatedEvent['data'], msg: Message) {
+
+    }
+}
