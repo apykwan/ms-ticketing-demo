fd90f2a699fe9e561c781bf5524cf764523c2ed4
關沛宇 <apykwan@hotmail.com>
2026-01-08 06:45:59 -0800

    implement onSumbit function on create ticket form

--- client/app/tickets/new/page.tsx
+++ client/app/tickets/new/page.tsx
@@ -1,16 +1,54 @@
+'use client';
+
+import { useState } from 'react';
+
+import useRequest from '@/hooks/use-request';
+
 export default function NewTicketPage () {
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+
+    const{ doRequest, errors } = useRequest({
+        url: '/api/tickets',
+        method: 'post',
+        body: { title, price },
+        onSuccess: (ticket) => console.log(ticket)
+    });
+
+    function onBlur() {
+        const value = parseFloat(price);
+        if (isNaN(value)) return;
+
+        setPrice(value.toFixed(2));
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+
+        doRequest();
+    }
     return (
-        <div className="p-3">
+        <div>
             <h1>Create a Ticket</h1>
-            <form>
+            <form className="p-3" onSubmit={onSubmit}>
                 <div className="mb-3">
                     <label className="form-label">Title</label>
-                    <input className="form-control" />
+                    <input 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)} 
+                        className="form-control" 
+                    />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Price</label>
-                    <input className="form-control" />
+                    <input 
+                        value={price}
+                        onBlur={onBlur}
+                        onChange={e => setPrice(e.target.value)} 
+                        className="form-control" 
+                    />
                 </div>
+                {errors}
                 <button className="btn btn-primary w-100">Submit</button>
             </form>
         </div>
